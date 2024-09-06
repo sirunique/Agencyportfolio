6146ed5c08130e32b71c35f799e851ebb0632beb
Ganiyu Hassan Taiwo <hassantaiwo554@gmail.com>
2024-09-06 23:44:50 +0100

    done

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 "use strict";
-
-const mobileToggle = document.querySelector("i");
-
-mobileToggle.addEventListener(
-  "click",
-  () => (document.querySelector(".nav__links").style.display = "block")
-);
