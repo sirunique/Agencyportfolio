91555ac66e67dfea8a24cae970f29a6a8d1ece87
Ganiyu Hassan Taiwo <hassantaiwo554@gmail.com>
2024-09-07 09:00:37 +0100

    updated

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const toggleBtn = document.querySelector(".toggle__btn");
+const navLinks = document.querySelector(".nav__links");
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+
+toggleBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  menu.classList.toggle("hide");
+  close.classList.toggle("show");
+});
