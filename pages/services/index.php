56d820142f40fd33364083fe3de5124859c4b106
Ganiyu Hassan Taiwo <hassantaiwo554@gmail.com>
2024-09-11 22:20:34 +0100

    service page updated

--- pages/services/index.php
+++ pages/services/index.php
@@ -2,9 +2,104 @@
 <?php include('./components/header.php'); ?>
 
 <!-- Page Content -->
-<section class="page-content">
-    <h1>Services Page</h1>
+<section class="section row section--1">
+    <img class='grid-img section_2-img' src="images/IMG-20240906-WA0012.jpg" alt="images">
+    <div class='element-align'>
+        <p>OUR SERVICES</p>
+        <h1>Our Thoughtful Offerings</h1>
+        <p>Our services are thoughtfully designed to meet the needs of both new and established businesses. We are also very flexible when it comes to creating the perfect package for you. Your best fit may be a more custom approach and fully customized package. For a more individual approach, please ask via mail.</p>
+        <a class='email-link' href="#">hello@polafijalkocreative.com</a>
+    </div>
+</section>
 
+<section class="section section--2" style="padding-bottom: 25px;">
+    <h2 style="font-size: 30px; margin: 20px;">Branding Offer</h2>
+    <img class='grid-img' src="images/IMG-20240906-WA0012.jpg" alt="images">
+    <div class="element-align">
+        <div style="display: flex; justify-content: space-between;">
+            <p>1-2 WEEKS</p>
+            <p>€ 650 / 700 USD</p>
+        </div>
+        <h1>Logo Design</h1>
+        <p>This package is designed for those who appreciate simplicity and sophistication in a compact approach. 'Logo Design' includes primary logo design and/or logo mark, providing an essential branding element that can be used across various platforms to create a cohesive and recognizable identity.
+        </p>
+
+        <p>What's included:</p>
+        <ul style="display: flex; flex-direction: column; gap: 0.5rem;">
+            <li>Creative Direction</li>
+            <li>Copyright Transfer</li>
+            <li>1-2 Logos</li>
+        </ul>
+    </div>
+</section>
+
+<section class="section section--3" style="padding-bottom: 25px;">
+    <img class='grid-img' src="images/IMG-20240906-WA0012.jpg" alt="images">
+    <div class="element-align">
+        <div style="display: flex; justify-content: space-between;">
+            <p>3-4 WEEKS</p>
+            <p>€ 650 / 700 USD</p>
+        </div>
+        <h1>Minimal Branding</h1>
+        <p>This package is designed for those seeking minimal and purposeful aesthetics to align with their vision. 'Minimal’ provides you with everything you need to start your business’s branding journey. This is a great option for brands starting out with a smaller budget but craving to create something significant.
+        </p>
+
+        <p>What's included:</p>
+        <ul style="display: flex; flex-direction: column; gap: 0.5rem;">
+            <li>Brandboard</li>
+            <li>Basic Brand Guidelines</li>
+            <li>Typography Suite</li>
+            <li>Color Palette</li>
+            <li>2-3 Logo Submarks</li>
+            <li>Secondary Logotype</li>
+            <li>Primary Logotype</li>
+            <li>Creative Direction</li>
+            <li>Branding Questionnaire</li>
+            <li>Copyright Transfer</li>
+
+        </ul>
+    </div>
+</section>
+
+<section class="section section--4" style="padding-bottom: 25px;">
+    <img class='grid-img' src="images/IMG-20240906-WA0012.jpg" alt="images">
+    <div class="element-align">
+        <div style="display: flex; justify-content: space-between;">
+            <p>8-12 Weeks</p>
+            <p>€ 650 / 700 USD</p>
+        </div>
+        <h1>Creative Branding</h1>
+        <p>A strategic branding package for those who are ready to invest in a delightful and consistent identity, and joyfully bring their vision to life in its fullest manifestation. If you are looking to truly explore your ideas and craft meanigful identity during a detailed process of exploring the spirit of your brand, this package is for you.
+        </p>
+
+        <p>What's included:</p>
+        <ul style="display: flex; flex-direction: column; gap: 0.5rem;">
+            <li>Creative Direction</li>
+            <li>Copyright Transfer</li>
+            <li>1-2 Logos</li>
+            <li>Brandboard</li>
+            <li>Basic Brand Guidelines</li>
+            <li>Typography Suite</li>
+            <li>Color Palette</li>
+            <li>2-3 Logo Submarks</li>
+            <li>Secondary Logotype</li>
+            <li>Primary Logotype</li>
+            <li>Creative Direction</li>
+            <li>Branding Questionnaire</li>
+            <li>Copyright Transfer</li>
+        </ul>
+    </div>
+</section>
+
+
+<section class="section row section--5">
+    <img src="images/IMG-20240906-WA0012.jpg" alt="images">
+    <div>
+        <p>OUR SERVICES</p>
+        <h1>Our Thoughtful Offerings</h1>
+        <p>Our services are thoughtfully designed to meet the needs of both new and established businesses. We are also very flexible when it comes to creating the perfect package for you. Your best fit may be a more custom approach and fully customized package. For a more individual approach, please ask via mail.</p>
+        <a href="#">hello@polafijalkocreative.com</a>
+    </div>
 </section>
 
 <!-- Footer -->
