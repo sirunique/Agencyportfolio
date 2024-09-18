f6236e7f3f6526bd2f54e5a7bdb2ab84d5f829f9
Ganiyu Hassan Taiwo <hassantaiwo554@gmail.com>
2024-09-18 08:07:10 +0100

    Lazy loading done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
+// DOM
 const header = document.querySelector(".header");
 const navbar = document.querySelector(".navbar");
 const toggleBtn = document.querySelector(".toggle__btn");
@@ -6,29 +7,60 @@ const navLinks = document.querySelector(".nav__links");
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
 const allSections = document.querySelectorAll(".section");
+const imgTarget = document.querySelectorAll("img[data-src]");
+// DOM
 
+// Mobile nav toggle
 toggleBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   menu.classList.toggle("hide");
   close.classList.toggle("show");
 });
+// Mobile nav toggle
 
 // Reveal sections
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section--hidden");
-  observer.unobserve(entry.target);
+const sectionReveal = () => {
+  const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove("section--hidden");
+    observer.unobserve(entry.target);
+  };
+
+  const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+  });
+
+  allSections.forEach((section) => {
+    sectionObserver.observe(section);
+    section.classList.add("section--hidden");
+  });
 };
-
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
-});
-
-allSections.forEach((section) => {
-  sectionObserver.observe(section);
-  section.classList.add("section--hidden");
-});
+sectionReveal();
 
 // Lazy loading images
+const lazyLoading = () => {
+  const loadingImg = function (entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    // Replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+    // Remove the blur effect
+    entry.target.addEventListener("load", () =>
+      entry.target.classList.remove("lazy-img")
+    );
+
+    observer.unobserve(entry.target);
+  };
+
+  const imgObserver = new IntersectionObserver(loadingImg, {
+    root: null,
+    threshold: 0,
+  });
+
+  imgTarget.forEach((img) => imgObserver.observe(img));
+};
+lazyLoading();
