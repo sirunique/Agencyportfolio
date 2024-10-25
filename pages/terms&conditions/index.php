89b89594e2f367787181b67bd12b59dda8cb7644
PrimeXV <hassantaiwo554@gmail.com>
2024-10-25 11:49:27 +0100

    Terms and condition page completed

--- pages/terms&conditions/index.php
+++ pages/terms&conditions/index.php
@@ -5,8 +5,69 @@ include('./pages/terms&conditions/terms&conditions_css.php');
 ?>
 
 <!-- Terms and conditions -->
-<section>
-    <p>Terms and conditions</p>
+<section class="element-align desktop">
+    <h1>Terms and Conditions for DeenspoHub</h1>
+
+    <ul class="lists">
+        <li class="list">
+            <h3>1. Introduction</h3>
+            <p>Welcome to DeenspoHub. These Terms and Conditions govern your use of our services and software products. By engaging with DeenspoHub, you agree to comply with these terms.
+            </p>
+        </li>
+        <li class="list">
+            <h3>2. Services</h3>
+            <p>DeenspoHub provides specialized services to Small and Medium-sized Enterprises (SMEs) including, but not limited to, business consulting, software sales, and support. Our software products are designed to enhance the efficiency and productivity of your business operations.</p>
+        </li>
+        <li class="list">
+            <h3>3. Software Licensing</h3>
+            <p>1. License Grant: Upon purchasing our software, you are granted a non-exclusive, non-transferable license to use the software according to the terms specified in the license agreement.
+            </p>
+            <p>2. Restrictions: You may not modify, reverse engineer, or redistribute the software without express written permission from DeenspoHub.</p>
+        </li>
+        <li class="list">
+            <h3>4. Payment Terms</h3>
+            <p>1. Fees: All service and software fees are outlined in your purchase agreement. Fees are subject to change with prior notice.</p>
+            <p>2. Payment Schedule: Payments are due according to the schedule specified in the invoice or contract. Late payments may incur additional charges.</p>
+        </li>
+        <li class="list">
+            <h3>5. Service Use</h3>
+            <p>1. User Responsibilities: Users are responsible for maintaining the confidentiality of their account information and ensuring that all activities under their account comply with these terms.</p>
+            <p>2. Prohibited Activities: Users may not use our services or software for unlawful activities or in a manner that disrupts or damages our services</p>
+        </li>
+        <li class="list">
+            <h3>6. Support and Maintenance</h3>
+            <p>DeenspoHub provides support and maintenance for its software products as outlined in the support agreement. This may include updates, bug fixes, and technical assistance.</p>
+        </li>
+        <li class="list">
+            <h3>7. Intellectual Property</h3>
+            <p>All intellectual property rights related to DeenspoHub’s software and services remain the property of DeenspoHub. Users may not use our intellectual property without prior written consent.</p>
+        </li>
+        <li class="list">
+            <h3>8. Confidentiality</h3>
+            <p>Both parties agree to maintain the confidentiality of any proprietary information disclosed during the course of the business relationship.</p>
+        </li>
+        <li class="list">
+            <h3>9. Limitation of Liability</h3>
+            <p>DeenspoHub is not liable for any indirect, incidental, or consequential damages arising from the use of our services or software. Our total liability is limited to the amount paid for the services or software.</p>
+        </li>
+        <li class="list">
+            <h3>10. Termination</h3>
+            <p>Either party may terminate the agreement with prior notice. Upon termination, users must cease using the software and return or destroy all copies.</p>
+        </li>
+        <li class="list">
+            <h3>11. Governing Law</h3>
+            <p>These terms are governed by the laws of the jurisdiction in which DeenspoHub operates. Any disputes will be resolved in the appropriate courts of this jurisdiction.</p>
+        </li>
+        <li class="list">
+            <h3>12. Amendments</h3>
+            <p>DeenspoHub reserves the right to amend these terms at any time. Users will be notified of any significant changes.</p>
+        </li>
+        <li class="list">
+            <h3>13. Contact Information</h3>
+            <p>For any questions or concerns regarding these terms, please contact us at [contact@deenspo24@gmail.com).</p>
+            <p>By using DeenspoHub’s services and software, you acknowledge that you have read, understood, and agree to be bound by these Terms and Conditions.</p>
+        </li>
+    </ul>
 </section>
 <!-- Terms and conditions -->
 
--- pages/terms&conditions/terms&conditions_css.php
+++ pages/terms&conditions/terms&conditions_css.php
@@ -1,1 +1,37 @@
-<style></style>
+<style>
+    .element-align,
+    .list {
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+    }
+
+    .lists {
+        display: flex;
+        flex-direction: column;
+        gap: 2.5rem;
+    }
+
+    h1 {
+        font-size: 27px;
+        font-weight: 250;
+    }
+
+    h3 {
+        font-size: 22px;
+        font-weight: 200;
+    }
+
+    p {
+        font-family: 13px;
+    }
+
+    /* Desktop and Tablet */
+    @media only screen and (min-width: 780px) {
+        .element-align {
+            padding: 15rem 18rem;
+        }
+    }
+
+    /* Desktop and Tablet */
+</style>
