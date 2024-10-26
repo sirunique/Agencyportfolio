64e2d47467dd1f5599e2e486ef4c0ef88a0eec71
PrimeXV <hassantaiwo554@gmail.com>
2024-10-26 07:49:38 +0100

    Privacy page completed

--- pages/privacy_policy/index.php
+++ pages/privacy_policy/index.php
@@ -5,8 +5,82 @@ include('./pages/privacy_policy/privacy_policy_css.php');
 ?>
 
 <!-- Privacy and Policy -->
-<section>
-    <p>Privacy and Policy</p>
+<section class="element-align lists">
+    <h1>Privacy and Policy</h1>
+    <div class="introduction list">
+        <h2>Introduction</h2>
+        <p>This Privacy Policy outlines the methods by which your personal information is collected, used, and shared when you visit or make a purchase from <a href="">www.deenspohub.com</a>, owned and operated by Maryann Felix.</p>
+    </div>
+
+    <div class="agreement list">
+        <h2>Agreement to Privacy Policy</h2>
+        <p class="gap">By visiting, viewing, or using our website, you agree to be bound by the terms outlined in this Privacy Policy</p>
+
+        <ul class="lists">
+            <li class="list">
+                <h3>PERSONAL INFORMATION WE COLLECT</h3>
+                <p>While using our website, we may request that you provide us with certain personally identifiable information that can be used to contact or identify you. This information may include:</p>
+                <ul class="sub-list">
+                    <li>- First and last name</li>
+                    <li>- Email address</li>
+                    <li>- Social media details</li>
+                    <li>- Location information</li>
+                    <li>- Any additional information you provide through contact forms, email, or other communication methods</li>
+                </ul>
+            </li>
+            <li class="list">
+                <h3>USE OF YOUR PERSONAL DATA</h3>
+                <p>We may use your personal data for the following purposes:</p>
+                <ul class="sub-list">
+                    <li>- To contact you via email or other forms of electronic communication</li>
+                    <li>- To respond to inquiries, reviews, or other feedback you submit</li>
+                    <li>- To interact with you on social media</li>
+                    <li>- To fulfill orders placed through the site, including processing payment information, arranging shipping, and providing invoices and order confirmations</li>
+                    <li>- To request feedback from you</li>
+                    <li>- To administer contests or giveaways</li>
+                    <li>- For purposes such as data analysis, identifying usage trends, and evaluating and improving our services, products, and marketing</li>
+                    <li>- To assist law enforcement as necessary</li>
+                </ul>
+            </li>
+            <li class="list">
+                <h3>DATA RETENTION</h3>
+                <p>We retain personal data for as long as necessary to fulfill our legitimate business purposes, comply with legal obligations, or until you request the deletion of your data.</p>
+            </li>
+            <li class="list">
+                <h3>INTERNATIONAL DATA</h3>
+                <p>Our website is proudly hosted on state-of-the-art servers located in Nigeria. If you reside outside of Nigeria, please note that your data will be securely transferred and stored internationally on these servers. By using our site and submitting information through our contact form, you consent to this seamless and secure transfer, processing, and storage of your information in Nigeria. We assure you that we uphold the highest standards of data protection and privacy to ensure your information is handled with the utmost care and security.</p>
+            </li>
+            <li class="list">
+                <h3>SENSITIVE DATA</h3>
+                <p>We kindly ask that you refrain from submitting any sensitive data to us through public postings, email correspondence, or any other means, including but not limited to social security numbers, health or genetic information, and details regarding your ethnic origin, religious beliefs, or criminal history. If you choose to provide us with such information, your submission will be considered as your consent to its use, storage, and processing in accordance with our privacy policy.</p>
+                <p>We may share your Personal Information to comply with relevant laws and regulations, respond to subpoenas, search warrants, or other lawful requests for information we receive, or to safeguard our rights.</p>
+            </li>
+            <li class="list">
+                <h3>SHARING YOUR PERSONAL INFORMATION</h3>
+                <p>We may disclose your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant, or other lawful request for information we receive, or to protect our rights</p>
+            </li>
+            <li class="list">
+                <h3>YOUR RIGHT</h3>
+                <p>If you are a resident of Nigeria, you have the right to access the personal information we hold about you and request that it be corrected, updated, or deleted. To exercise this right, please contact us.</p>
+            </li>
+            <li class="list">
+                <h3>SECURITY OF YOUR PERSONAL DATA</h3>
+                <p>The security of your personal data is of utmost importance to us. However, please be aware that no method of transmission over the Internet or electronic storage is completely secure. While we employ commercially acceptable measures to protect your personal data, we cannot guarantee its absolute security</p>
+            </li>
+            <li class="list">
+                <h3>CHANGES</h3>
+                <p>We may update this Privacy Policy periodically to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will notify you of any changes by posting the updated Privacy Policy on this page</p>
+            </li>
+            <li class="list">
+                <h3>CONTACT US</h3>
+                <p>If you have any questions concerning the privacy policy, please contact us</p>
+            </li>
+            <li class="list">
+                <h3>DEENSPOHUB</h3>
+                <a href="">deenspohub24@gmail.com</a>
+            </li>
+        </ul>
+    </div>
 </section>
 <!-- Privacy and Policy -->
 
--- pages/privacy_policy/privacy_policy_css.php
+++ pages/privacy_policy/privacy_policy_css.php
@@ -1,1 +1,54 @@
-<style></style>
+<style>
+    .element-align {
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+        padding: 5rem 1.5rem;
+    }
+
+    .list {
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+    }
+
+    .lists {
+        display: flex;
+        flex-direction: column;
+        gap: 2.5rem;
+    }
+
+    .sub-list {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    }
+
+    h1 {
+        font-size: 27px;
+        font-weight: 250;
+    }
+
+    h3 {
+        font-size: 22px;
+        font-weight: 200;
+    }
+
+    p {
+        font-family: 13px;
+    }
+
+    .gap {
+        padding-bottom: 2rem;
+    }
+
+
+    /* Desktop and Tablet */
+    @media only screen and (min-width: 780px) {
+        .element-align {
+            padding: 15rem 18rem;
+        }
+    }
+
+    /* Desktop and Tablet */
+</style>
