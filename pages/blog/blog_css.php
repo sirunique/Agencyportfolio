9add5806fe6ec3ed3c1c12f27ceac9654630313c
PrimeXV <hassantaiwo554@gmail.com>
2024-10-30 03:53:13 +0100

    Blog page completed

--- pages/blog/blog_css.php
+++ pages/blog/blog_css.php
@@ -1,1 +1,58 @@
-<style></style>
+<style>
+    .blog--page {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 4rem;
+        padding: 5rem 2rem;
+    }
+
+    .slider-img {
+        height: 200px;
+        width: 400px;
+    }
+
+    .align,
+    .blog {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+
+    .slider {
+        display: flex;
+        flex-direction: column;
+        gap: 3rem;
+    }
+
+    h2 {
+        font-size: 2.5rem;
+        /* padding-bottom: 0.5rem; */
+    }
+
+    h1 {
+        font-size: 20px;
+        font-weight: 300;
+    }
+
+    /* Desktop view */
+    @media only screen and (min-width: 768px) {
+        .blog--page {
+            padding: 8rem 10rem;
+            gap: 6rem;
+        }
+
+        .blog {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+        }
+
+        .slider {
+            flex-direction: row;
+        }
+
+    }
+
+    /* Desktop view */
+</style>
--- pages/blog/index.php
+++ pages/blog/index.php
@@ -5,8 +5,43 @@ include('./pages/blog/blog_css.php');
 ?>
 
 <!-- Page Content -->
-<section class="section">
-    <h1>This is the Blog post</h1>
+<section class="blog--page">
+    <div class="blog">
+        <div class="grid-item">
+            <h2>Our Blog</h2>
+            <p>Agency Portfolio</p>
+        </div>
+        <div class="grid-item">
+            <h1>New product features, technology,
+                the latest gist & happening in DEESNPOHUB.</h1>
+        </div>
+    </div>
+
+    <div class="slider">
+        <div class="align grid-item">
+            <img class="slider-img" src="images/celesteBrand.jpg" alt="celeste">
+            <h1>Discover the available plans with their price to Naira</h1>
+            <a href="services.php">
+                <p>Discover the available plans with their price to Naira</p>
+            </a>
+        </div>
+        <div class="align grid-item">
+            <img class="slider-img" src="images/myrawoman.jpg" alt="celeste">
+            <h1>Discover the available plans with their price to Naira</h1>
+            <a href="services.php">
+                <p>Discover the available plans with their price to Naira</p>
+            </a>
+        </div>
+        <div class="align grid-item">
+            <img class="slider-img" src="images/samuelCray.jpg" alt="celeste">
+            <h1>Discover the available plans with their price to Naira</h1>
+            <a href="services.php">
+                <p>Discover the available plans with their price to Naira</p>
+            </a>
+        </div>
+
+    </div>
+
 </section>
 <!-- Page Content -->
 
